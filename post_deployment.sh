efd04cb90c38b7c87004d5ead5ddd00a7b4151a0
topogigiovanni <topogigiovanni@gmail.com>
2020-05-05 23:30:38 -0300

    Adjust circleci

--- post_deployment.sh
+++ post_deployment.sh
@@ -4,6 +4,6 @@ sudo npm install
 echo "building…"
 sudo npm run build:production
 echo "start server…"
-sudo pm2 startOrReload ecosystem.config.js --update-env --only piral-server-prd
+sudo pm2 startOrReload ../ecosystem.config.js --update-env --only piral-server-prd
 echo "started server. ending SSH session.."
 exit
